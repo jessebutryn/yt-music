bc084de59023fe9fec100a459c58d145c81ee745
jesse_b <jesse_b@mothership.localdomain>
2022-04-02 20:55:03 -0600

    initial commit

---
+++ bin/yt-music.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+[[ $DEBUG == true ]] && set -x
+#######################################
+if [[ -f "${0%/*}/../lib/config" ]]; then
+	. "${0%/*}/../lib/config"
+else
+	printf '%s\n' 'ERROR! Could not find lib/config' >&2
+	exit 1
+fi
+#######################################
+max_bg_procs () {
+	local max_number=$((0 + ${1:-0}))
+	while :; do
+		local current_number=$(jobs -pr | wc -l)
+		if [[ $current_number -lt $max_number ]]; then
+			break
+		fi
+		sleep 2
+	done
+}
+main () {
+    dos2unix "$url_file" &>/dev/null
+        file=$(youtube-dl -o "${output_dir}/%(title)s" --get-filename "$url")
+        file="${file}.mp3"
+        youtube-dl "${dl_opts[@]}" -o "${output_dir}/%(title)s.%(ext)s" "$url" >/dev/null
+        if [[ ! -f "$file" ]]; then
+            printf '%s\n' "$fail_file"
+        fi
+}
+#######################################
+mapfile -t urls < "$url_file"
+for url in "${urls[@]}"; do
+    max_bg_procs 5
+    main &
+done
+wait
+echo > "$url_file"
